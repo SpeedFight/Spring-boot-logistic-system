a425a4d99341d65e48e860e7a4c4141eccf2a186
SF <speedfight_2@wp.pl>
2018-10-10 20:03:27 +0200

    fix test names and fill enum for raport type

--- src/main/java/com/bootLogisticSystem/model/RaportType.java
+++ src/main/java/com/bootLogisticSystem/model/RaportType.java
@@ -1,5 +1,28 @@
 package com.bootLogisticSystem.model;
 
 public enum RaportType {
+	A("a","Ilość zamówień łącznie"),
+	B("b","Ilość zamówień do klienta o wskazanym identyfikatorze"),
+	C("c","Łączna kwota zamówień"),
+	D("d","Łączna kwota zamówień do klienta o wskazanym identyfikatorze"),
+	E("e","Lista wszystkich zamówień"),
+	F("f","Lista zamówień do klienta o wskazanym identyfikatorze"),
+	G("g","Średnia wartość zamówienia"),
+	H("h","Średnia wartość zamówienia do klienta o wskazanym identyfikatorze");
+	
+	private final String code;
+	private final String description;
+	
+	private RaportType(String code, String description) {
+		this.code = code;
+		this.description = description;
+	}
 
+	public String getCode() {
+		return code;
+	}
+
+	public String getDescription() {
+		return description;
+	}
 }
--- src/test/java/com/bootLogisticSystem/logic/args/validators/ValidateClientIdTest.java
+++ src/test/java/com/bootLogisticSystem/logic/args/validators/ValidateClientIdTest.java
@@ -6,6 +6,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ValidateRaportType {
+public class ValidateClientIdTest {
 
+	
+	
 }
