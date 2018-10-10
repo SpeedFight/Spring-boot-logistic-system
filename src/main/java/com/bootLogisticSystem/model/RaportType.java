af46a1debb049e2d4a640dbed4bae5d6b43c42d5
SF <speedfight_2@wp.pl>
2018-10-10 20:08:33 +0200

    add isRequireClientId to args enum

--- src/main/java/com/bootLogisticSystem/model/RaportType.java
+++ src/main/java/com/bootLogisticSystem/model/RaportType.java
@@ -1,21 +1,23 @@
 package com.bootLogisticSystem.model;
 
 public enum RaportType {
-	A("a","Ilość zamówień łącznie"),
-	B("b","Ilość zamówień do klienta o wskazanym identyfikatorze"),
-	C("c","Łączna kwota zamówień"),
-	D("d","Łączna kwota zamówień do klienta o wskazanym identyfikatorze"),
-	E("e","Lista wszystkich zamówień"),
-	F("f","Lista zamówień do klienta o wskazanym identyfikatorze"),
-	G("g","Średnia wartość zamówienia"),
-	H("h","Średnia wartość zamówienia do klienta o wskazanym identyfikatorze");
+	A("a", "Ilość zamówień łącznie", false),
+	B("b", "Ilość zamówień do klienta o wskazanym identyfikatorze", true),
+	C("c", "Łączna kwota zamówień", false),
+	D("d", "Łączna kwota zamówień do klienta o wskazanym identyfikatorze", true),
+	E("e", "Lista wszystkich zamówień", false),
+	F("f", "Lista zamówień do klienta o wskazanym identyfikatorze", true),
+	G("g", "Średnia wartość zamówienia", false),
+	H("h", "Średnia wartość zamówienia do klienta o wskazanym identyfikatorze", true);
 	
 	private final String code;
 	private final String description;
+	private final boolean requireClientId;
 	
-	private RaportType(String code, String description) {
+	private RaportType(String code, String description, boolean requireClientId) {
 		this.code = code;
 		this.description = description;
+		this.requireClientId = requireClientId;
 	}
 
 	public String getCode() {
@@ -24,5 +26,9 @@ public enum RaportType {
 
 	public String getDescription() {
 		return description;
+	}
+
+	public boolean isRequireClientId() {
+		return requireClientId;
 	}
 }
