eb0310c4a1bd6b3a2a7e607442993200938fa0a4
SF <speedfight_2@wp.pl>
2018-10-10 23:34:25 +0200

    add 2 test to output file validator

--- src/main/java/com/bootLogisticSystem/logic/args/validators/ValidateOutputPath.java
+++ src/main/java/com/bootLogisticSystem/logic/args/validators/ValidateOutputPath.java
@@ -1,6 +1,9 @@
 package com.bootLogisticSystem.logic.args.validators;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import com.bootLogisticSystem.exception.InvalidParameterException;
 import com.bootLogisticSystem.model.RaportType;
@@ -9,7 +12,30 @@ public class ValidateOutputPath {
 
 	public ValidateOutputPath() {};
 	
-	public File  validate(String outputPath) {		
-		return null;
+	public File  validate(String outputPath) throws InvalidParameterException{		
+		 File file = new File(outputPath);
+		 Path path = Paths.get(outputPath);
+		 
+//		if (Files.isReadable(path) && Files.isWritable(path)) {
+//			throw new InvalidParameterException("Path to save output file: " + outputPath + " is unacessable.");
+//		}
+		 
+		
+//		if (!file.isDirectory())
+//			   if(!file.getParentFile().mkdirs()) {
+//				   throw new InvalidParameterException("Can't create directories to: " + outputPath);
+//			   }
+//		if (file.exists()){
+//			//overwrite
+//		}
+		 try {
+			 if(!Files.exists(path)) {
+				 Files.createDirectories(path.getParent());
+			 }			
+		} catch (Exception e) {
+			throw new InvalidParameterException(e.getMessage());
+		}
+			
+		return file;
 	}
 }
--- src/test/java/com/bootLogisticSystem/logic/args/validators/ValidateOutputPathTest.java
+++ src/test/java/com/bootLogisticSystem/logic/args/validators/ValidateOutputPathTest.java
@@ -1,11 +1,76 @@
 package com.bootLogisticSystem.logic.args.validators;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.bootLogisticSystem.exception.InvalidParameterException;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ValidateOutputPathTest {
+	
+	private static ValidateOutputPath validateOutputPath;
+	
+	@BeforeClass
+	public static void setUp() {
+		validateOutputPath = new ValidateOutputPath();
+	}
+	
+	@Test
+	public void createNewFile() throws InvalidParameterException, IOException {
+		String path = "src/test/testResources/output.xml";
+		
+		File createdFile = validateOutputPath.validate(path);
+		
+		assertTrue(testWriteToFile(createdFile,"testString"));		
+	}
+	
+	@Test
+	public void createNewFileAndDirectory() throws InvalidParameterException, IOException {
+		String path = "src/test/testResources/newDirectory/output.xml";
+		
+		File createdFile = validateOutputPath.validate(path);
+		
+		assertTrue(testWriteToFile(createdFile,"testString"));	
+		
+		createdFile.getParentFile().delete();
+	}	
+	
+	
+	private boolean testWriteToFile(File file, String testString) throws IOException {
+		Writer writer = new FileWriter(file);
+		writer.write("testString");
+		writer.close();
+		
+		Reader reader = new FileReader(file);
+		
+		int i;
+		String fileContent = "";
+		while((i = reader.read()) != -1) {
+			fileContent = fileContent + (char)i;
+		}
+		reader.close();
+		
+		if(!file.delete()) { 
+			throw new IOException("Can't delete test file: " + file);
+        }
+		
+		return (testString.compareTo(fileContent) == 0);
+	}
 
 }
