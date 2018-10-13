004b60170e71e551f7ceca7570527c40b8436b9b
SF <speedfight_2@wp.pl>
2018-10-13 17:51:01 +0200

    organize imports

--- src/main/java/com/bootLogisticSystem/logic/raportGenerator/customOptions/RequestListRaportCsvCustomWriter.java
+++ src/main/java/com/bootLogisticSystem/logic/raportGenerator/customOptions/RequestListRaportCsvCustomWriter.java
@@ -4,9 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import com.bootLogisticSystem.entity.GenerateAble;
 import com.bootLogisticSystem.logic.raportGenerator.RaportWriter;
 import com.fasterxml.jackson.annotation.JsonProperty;
--- src/main/java/com/bootLogisticSystem/logic/raportGenerator/customOptions/RequestListRaportXmlCustomWriter.java
+++ src/main/java/com/bootLogisticSystem/logic/raportGenerator/customOptions/RequestListRaportXmlCustomWriter.java
@@ -4,9 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import com.bootLogisticSystem.dto.RequestDtoJacksonXml;
 import com.bootLogisticSystem.entity.GenerateAble;
 import com.bootLogisticSystem.entity.Request;
