c968c22a6676b06a365524eb0dcabd66d606c5db
SF <speedfight_2@wp.pl>
2018-10-14 13:50:58 +0200

    Add javadoc

--- src/main/java/com/bootLogisticSystem/logic/dataReader/DataParser.java
+++ src/main/java/com/bootLogisticSystem/logic/dataReader/DataParser.java
@@ -8,8 +8,25 @@ import com.bootLogisticSystem.entity.GenerateAble;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
+/**
+ * Provide specific data type parse.
+ * 
+ * @author Karol Łukasiewicz
+ *
+ */
 public interface DataParser {
 
-	<T extends GenerateAble> List<T> parse(File fileToParse, Class<T> inputDataPojo) throws JsonParseException, JsonMappingException, IOException;
+	/**
+	 * Parse input file to obtain list with elements inside.
+	 * 
+	 * @param fileToParse Path to parsed file
+	 * @param inputDataPojo Class of element inside parsed file. File class must extends GenerateAble.
+	 * @return List with parsed files.
+	 * @throws JsonParseException
+	 * @throws JsonMappingException
+	 * @throws IOException
+	 */
+	<T extends GenerateAble> List<T> parse(File fileToParse, Class<T> inputDataPojo)
+			throws JsonParseException, JsonMappingException, IOException;
 
 }
