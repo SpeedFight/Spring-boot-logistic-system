05c4f74e4bc127e4798ead44492d525c3220b406
SF <speedfight_2@wp.pl>
2018-10-14 19:19:14 +0200

    add template

--- src/main/java/com/bootLogisticSystem/logic/raportGenerator/RaportGenerator.java
+++ src/main/java/com/bootLogisticSystem/logic/raportGenerator/RaportGenerator.java
@@ -1,5 +1,43 @@
 package com.bootLogisticSystem.logic.raportGenerator;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.bootLogisticSystem.entity.GenerateAble;
+import com.bootLogisticSystem.model.RaportType;
+import com.bootLogisticSystem.repository.RequestRepository;
+
+@Component
 public class RaportGenerator {
 
+	@Autowired
+	private RequestRepository orderRepository;
+	
+	public <T extends GenerateAble> List<T> generate(RaportType raportType){
+		
+		switch (raportType) {
+		case A:		
+			break;
+		case B:
+			break;
+		case C:
+			break;
+		case D:
+			break;
+		case E:
+			break;
+		case F:
+			break;
+		case G:
+			break;
+		case H:
+			break;
+		default:
+			break;
+		}
+		
+		return null;
+	}
 }
