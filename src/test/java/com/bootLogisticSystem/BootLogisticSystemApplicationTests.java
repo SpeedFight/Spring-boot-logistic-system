3b2b8975e6584ebbcab775f3dd334347e417cbd4
SF <speedfight_2@wp.pl>
2018-10-15 14:09:04 +0200

    add windows compile and run

--- src/test/java/com/bootLogisticSystem/BootLogisticSystemApplicationTests.java
+++ src/test/java/com/bootLogisticSystem/BootLogisticSystemApplicationTests.java
@@ -2,12 +2,30 @@ package com.bootLogisticSystem;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BootLogisticSystemApplicationTests {
+
+	@Configuration
+	@EnableJpaRepositories("com.bootLogisticSystem.repository")
+	@EntityScan("com.bootLogisticSystem.entity")
+	@ComponentScan("com.bootLogisticSystem")
+	static class ContextConfiguration {
+
+		@Primary // may omit this if this is the only SomeBean defined/visible
+	    public static void main(String[] args) {
+	        SpringApplication.run(BootLogisticSystemApplicationTests.class, args);
+	    }
+	}
 
 	@Test
 	public void contextLoads() {
